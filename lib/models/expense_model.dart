070fcb2b1c49c03a70e1e9923ffca665e5b82992
Tharidu herath <thariduherath7@gmail.com>
2025-12-09 10:09:17 +0530

    income and expence toggle

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,49 @@
+// enumm for expense catogory
+
+import 'dart:ui';
+
+enum ExpenseCategory { food, transport, health, shopping, subscription }
+
+// category images
+
+final Map<ExpenseCategory, String> expenseCategoryImages = {
+  ExpenseCategory.food: "aassets/images/restaurant.png",
+  ExpenseCategory.transport: "assets/images/car.png",
+  ExpenseCategory.health: "assets/images/health.png",
+  ExpenseCategory.shopping: "assets/images/bag.png",
+  ExpenseCategory.subscription: "assets/images/bill.png",
+};
+
+// category colors
+
+
+final Map<ExpenseCategory, Color> expenseCategoryColor = {
+   ExpenseCategory.food: const Color(0xFFE57373),
+  ExpenseCategory.transport: const Color(0xFF81C784),
+  ExpenseCategory.health: const Color(0xFF64B5F6),
+  ExpenseCategory.shopping: const Color(0xFFFFD54F),
+  ExpenseCategory.subscription: const Color(0xFF9575CD),
+};
+
+
+//model
+
+class ExpenseModel {
+  final int id;
+  final String title;
+  final double amount;
+  final ExpenseCategory category;
+  final DateTime date;
+  final DateTime time;
+  final String description;
+
+  ExpenseModel({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+    required this.time,
+    required this.description,
+  });
+}
--- lib/screens/add_new_screens.dart
+++ lib/screens/add_new_screens.dart
@@ -1,3 +1,5 @@
+import 'package:expenz/constants/colors.dart';
+import 'package:expenz/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -8,12 +10,90 @@ class AddNewScreen extends StatefulWidget {
 }
 
 class _AddNewScreenState extends State<AddNewScreen> {
+  // 0 = Expense, 1 = Income
+  int _selectedMethod = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _selectedMethod == 0 ? kRed : kGreen,
 
-      appBar: AppBar(
-        title: Text("Add new Screen"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.06,
+                decoration: BoxDecoration(
+                  color: kWhite,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                  children: [
+                    // -------------------------------- EXPENSE BUTTON --------------------------------
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedMethod = 0;
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: _selectedMethod == 0 ? kRed : kWhite,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 60,
+                          vertical: 10,
+                        ),
+                        child: Text(
+                          "Expense",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: _selectedMethod == 0 ? kWhite : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // -------------------------------- INCOME BUTTON --------------------------------
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedMethod = 1;
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: _selectedMethod == 1 ? kGreen : kWhite,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 60,
+                          vertical: 10,
+                        ),
+                        child: Text(
+                          "Income",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: _selectedMethod == 1 ? kWhite : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
